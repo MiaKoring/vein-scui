3e166d1a343047c0b59432aa768a9c2c2445a7a5
Mia Koring <koring.mia@gmail.com>
2026-01-14 20:41:55 +0100

    changed to automatically setup fields

--- Sources/VeinSCUI/Model.swift
+++ Sources/VeinSCUI/Model.swift
@@ -1,7 +1,7 @@
 import Vein
 import SwiftCrossUI
 
-@attached(member, names: named(init), named(id), named(setupFields), named(context), named(_getSchema), named(_fields), named(_fieldInformation), named(notifyOfChanges), named(_key), named(_PredicateHelper), named(_satisfiesConstraint))
+@attached(member, names: named(init), named(id), named(_setupFields), named(context), named(_getSchema), named(_fields), named(_fieldInformation), named(notifyOfChanges), named(_key), named(_PredicateHelper), named(_satisfiesConstraint))
 @attached(extension, conformances: PersistentModel, Sendable, SwiftCrossUI.ObservableObject)
 @attached(peer, names: arbitrary)
 public macro Model() = #externalMacro(
